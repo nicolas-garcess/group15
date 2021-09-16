67df60d516e9096b166dbd2dbfad123e51193588
nicolas-garcess <nicolasgarces95@gmail.com>
2021-09-15 21:06:58 -0500

    [feature][Nicolas] creado esquema de colleción de proyectos

---
+++ basedatos/projectScheme.js
@@ -0,0 +1,49 @@
+db.projects.insertOne({
+  idProyecto: 'proyecto002',
+  objetivos: {
+    objetivoGeneral: {
+      idObjetivo: 'proyecto002ObjG1',
+      descripcion: 'lorem ipsum dolo',
+      cumplido: false
+    },
+    objetivosEspecificos:
+      [
+        {
+          idObjetivo: 'proyecto002ObjE1',
+          descripcion: 'lorem ipsum dolo',
+          cumplido: false
+        },
+        {
+          idObjetivo: 'proyecto002ObjE2',
+          descripcion: 'lorem ipsum dolo',
+          cumplido: false
+        }
+      ]
+  },
+  presupuesto: 100000000,
+  fechaInicial: new Date('2021-05-01T08:00:00Z'),
+  fechaFinal: new Date('2021-010-01T08:00:00Z'),
+  directorProyecto: 'Andrés González',
+  estaDisponible: true,
+  avance: 10,
+  fase: 'inicio',
+  estudiantes: [
+    {
+      idEstudiante: 'estudiante003',
+      activo: true
+    },
+    {
+      idEstudiante: 'estudiante004',
+      activo: true
+    },
+  ],
+  investigadores: [
+    {
+      idInvestigador: 'investigador002',
+      activo: true
+    },
+  ],
+  notas: {
+    nota001: 'descripcionNota',
+  }
+})
