a3c4b83a980a03a77ac42ab295f5aca8ba36405c
nicolas-garcess <nicolasgarces95@gmail.com>
2021-10-03 18:04:19 -0500

    [feature][nicolas] queries to get students are configured

--- src/schema/students/query.js
+++ src/schema/students/query.js
@@ -1,6 +1,17 @@
+const student = require('../../models/students.model');
+
 const studentQueries = {
   greet(_, { name }) {
     return `Hello ${name}`;
+  },
+  async getStudents() {
+    return student.find();
+  },
+  async getStudentById(_, { id }) {
+    return student.findOne({ id });
+  },
+  async getStudentByEmail(_, { email }) {
+    return student.findOne({ email });
   },
 };
 
--- src/schema/students/type.js
+++ src/schema/students/type.js
@@ -1,6 +1,21 @@
 const studentTypes = `
     type Query {
-        greet(name: String!): String
+      greet(name: String!): String
+      getStudents: [Student]
+      getStudentById(id: String!): Student
+      getStudentByEmail(email: String!): Student
+    }
+
+    type Student {
+      id: String
+      nombre: String
+      carrera: String
+      celular: String
+      idProyecto: String
+      email: String
+      usuario: String
+      contrasena: String
+      activo: String
     }
 `;
 
