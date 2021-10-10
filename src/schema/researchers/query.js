feabfb473dfffeecd854e4924f159ac8ea1cd642
nicolas-garcess <nicolasgarces95@gmail.com>
2021-10-10 15:04:36 -0500

    Validation search student and researcher done

--- src/schema/researchers/query.js
+++ src/schema/researchers/query.js
@@ -1,11 +1,34 @@
+const { GraphQLError } = require('graphql');
 const Researcher = require('../../models/researchers.model');
+const { schemaUserId } = require('../validations');
 
 const researcherQueries = {
   async researchers() {
     return Researcher.find();
   },
   async researcher(_, { id }) {
-    return Researcher.findOne({ id });
+    const { error } = schemaUserId.validate(
+      { id },
+      { abortEarly: false },
+    );
+
+    if (error) {
+      throw new GraphQLError({
+        error: `${error}`,
+        wasSuccessful: false,
+      });
+    }
+
+    const researcher = await Researcher.findOne({ id });
+
+    if (researcher !== null) {
+      return researcher;
+    }
+
+    return new GraphQLError({
+      error: `The researcher Id ${id} does not exist`,
+      wasSuccessful: false,
+    });
   },
 };
 
