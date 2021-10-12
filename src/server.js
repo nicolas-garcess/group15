e03269ed6449bd1566a0cca601cabb40eeaf324c
nicolas-garcess <nicolasgarces95@gmail.com>
2021-10-11 19:58:26 -0500

    [fix][nicolas] fix bug when updating student and researcher

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
+require('dotenv').config();
 
 const app = express();
 
